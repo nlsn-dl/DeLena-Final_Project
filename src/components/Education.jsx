b970384046a7bbf0a55c156170b081961aeec13d
nlsn-dl <delenaferrer@gmail.com>
2026-04-18 16:23:22 +0800

    initial commit

---
+++ src/components/Education.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Education() {
+    return ( <section className="my-12">
+      <h2 className="text-3xl font-semibold text-blue-600">Education</h2>
+      <p className="text-lg mt-4">
+        I am a passionate web developer with a focus on building efficient and user-friendly applications.
+      </p>
+    </section>
+  );
+};
+
+export default Education;
